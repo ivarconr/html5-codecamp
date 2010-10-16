612b7c71e4d795025f081c34f67cf1dc170d7c2a
halrik <rikard.halvorsen@iteraconsulting.no>
2010-10-16 15:33:06 +0200

    ll

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,19 @@ $(function() {
    $('#addBlogPostLink').click(function() {
        $('#addPost').slideToggle(150, null);
    });
+   
+   
+   
+  // Replace the <textarea id="editor"> with an CKEditor
+  // instance, using default configurations.
+  CKEDITOR.replace( 'content',
+    {
+      extraPlugins : 'uicolor',
+      uiColor: '#DFD',
+      toolbar :
+      [
+        [ 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink' ],
+        [ 'UIColor' ]
+      ]
+    } );
 });
